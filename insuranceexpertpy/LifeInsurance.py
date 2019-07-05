4d8ed15157aa3684de71f852bbda246d0e3b598e
rkruger <rodrigo.kruger@fh.com.br>
2019-07-05 10:50:09 -0300

    Correction in Knowledge Engine rules

--- insuranceexpertpy/LifeInsurance.py
+++ insuranceexpertpy/LifeInsurance.py
@@ -320,7 +320,7 @@ class localizadaAreaRiscoNatural(Fact):
 
 class imovelEmConstrucao(Fact):
     """Imovel em construção?"""
-    emConstrução = Field(bool, mandatory=True, default=False)
+    emConstrucao = Field(bool, mandatory=True, default=False)
 
 
 class possuiSistemaDeMonitoramento(Fact):
@@ -444,83 +444,299 @@ class possuiPlanoDeSaude(Fact):
 
 
 class SeguroDeVida(KnowledgeEngine):
-
     insuranceScore = 0
 
     @Rule(Fact(cpf=True) | Fact(esportesRadicais=True)
           | Fact(pilotoCorrida=True)
           | Fact(drogas=True)
           | Fact(indenizacao=True)
-          | Fact(aeronaves=True))
-    def NegarSeguro(self):
+          | Fact(aeronaves=True)
+          | Fact(cirurgia=True)
+          | Fact(problemaCardiaco=True)
+          | Fact(terminal=True)
+          | Fact(vacinaMeningite=True)
+          | Fact(viagemGuerra=True)
+          | Fact(esportesTiro=True)
+          | Fact(deficiencia=True))
+    def regra001(self):
         self.declare(Fact(seguro_nao_possivel=True))
 
     @Rule(Fact(doencasGraves=True) & Fact(acompanhamento=True))
-    def DuplicarValor(self):
-        if (self.insuranceScore == 0):
+    def regra002(self):
+        if self.insuranceScore == 0:
             self.insuranceScore = 2
         else:
             self.insuranceScore *= 2
 
     @Rule(Fact(doencasGraves=True) & Fact(acompanhamento=False))
-    def NegarSeguro(self):
-        self.declare(Fact(seguro_nao_possivel=True))
+    def regra003(self):
+        print("Não é possível realizar um seguro para o cliente")
 
     @Rule(Fact(fuma=True)
           | Fact(bebe=True)
           | Fact(cep=True)
           | Fact(sedentario_=True)
-          | Fact(pagador=True))
-    def AumentarValor(self):
+          | Fact(pagador=True)
+          | Fact(cancerNaFamilia=True)
+          | Fact(vacinaPolio=True)
+          | Fact(viagemAfrica=True)
+          | Fact(desempregado_=True)
+          | Fact(sedentario_=True)
+          | Fact(obeso=True)
+          | Fact(artrose=True)
+          | Fact(medicamentoControlado=True)
+          | Fact(hepatite=True))
+    def regra004(self):
         self.insuranceScore += 1
 
     @Rule(Fact(idade=P(lambda idade: idade >= 60 and idade < 70)))
-    def AumentarValor(self):
+    def regra005(self):
         self.insuranceScore += 1
 
     @Rule(Fact(idade=P(lambda idade: idade >= 70)))
-    def NegarSeguro(self):
+    def regra006(self):
         self.declare(Fact(seguro_nao_possivel=True))
 
-    @Rule(Fact(diabetes=P(lambda diabetes: diabetes > normal)))
+    @Rule(Fact(diabetes=P(lambda diabetes: diabetes < 120)))
     def AumentarValor(self):
         self.insuranceScore += 1
 
-#    @Rule(Fact(diabetes=P(lambda diabetes: diabetes > muito acima do normal)))
-#    def NegarSeguro(self):
-#        print("Não é possível realizar um seguro para o cliente")
+    @Rule(Fact(diabetes=P(lambda diabetes: diabetes > 200)))
+    def regra007(self):
+        self.declare(Fact(seguro_nao_possivel=True))
 
     @Rule(Fact(profissao=True))
-    def AumentarValor(self):
+    def regra008(self):
         self.insuranceScore += 1
 
     @Rule(Fact(pressao=True) & Fact(acompanhamento=True))
-    def AumentarValor(self):
+    def regra009(self):
         self.insuranceScore += 1
 
     @Rule(Fact(pressao=True) & Fact(acompanhamento=False))
-    def NegarSeguro(self):
+    def regra010(self):
         self.declare(Fact(seguro_nao_possivel=True))
 
     @Rule(Fact(possuiFilhos_=True) & Fact(quantosFilhos=P(lambda possuiFilhos_: possuiFilhos_ > 0)))
-    def AumentoPercentual(self):
+    def regra011(self):
         self.insuranceScore += 1
 
     @Rule(Fact(casado=True))
-    def AumentarEOferecer(self):
+    def regra011(self):
         self.insuranceScore += 1
 
     @Rule(Fact(viagens=P(lambda viagens: viagens > 3)))
-    def AumentarValor(self):
+    def regra012(self):
         self.insuranceScore += 1
 
-# engine = SeguroDeVida()
-# engine.reset()
-#
-# engine.declare(Fact(cpf=input("CPF está negativado? ")))
-# # engine.declare(Fact(esportesRadicais = input("Praticante de esportes radicais? ")))
-# # engine.declare(Fact(pilotoCorrida = input("Piloto de Corrida? ")))
-# # engine.declare(Fact(drogas = input("Usuário de drogas? ")))
-# # engine.declare(Fact(recebeuIndenizacaoPorInvalidez = input("Já recebeu indenização por invalidez? ")))
-#
-# engine.run()
+    @Rule(Fact(neuro=True) & Fact(acompanhamento=True))
+    def regra013(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(neuro=True) & Fact(acompanhamento=False))
+    def regra014(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(carro=True))
+    def regra015(self):
+        self.declare(Fact(ofereca_seguro_carro=True))
+
+    @Rule(Fact(carro=True) & Fact(aceitar=True))
+    def regra016(self):
+        print("Continuar com peguntas sobre o carro")
+
+    @Rule(Fact(idade=P(lambda idade: idade < 25)))
+    def regra017(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(abs_ou_air=True))
+    def regra018(self):
+        self.insuranceScore -= 1
+
+    @Rule(Fact(frances_ou_chines=True)
+          | Fact(contraTerceiros=True)
+          | Fact(franquia_reduzida=True)
+          | Fact(roubo=True)
+          | Fact(incendio=True)
+          | Fact(garagem=True)
+          | Fact(carro_reserva=True)
+          | Fact(acidentesPessoais=True)
+          | Fact(condutorAcidente=True)
+          | Fact(indenizacaoFipe=True)
+          | Fact(sexo_=P(lambda sexo_: sexo_ == 1))
+          | Fact(veiculo_viagens=True)
+          | Fact(cobertura=P(lambda cobertura: cobertura == 3)))
+    def regra019(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(alemao_ou_japones=True)
+          | Fact(um_condutor=True)
+          | Fact(possuiRastreador=True)
+          | Fact(cobertura=P(lambda cobertura: cobertura == 1)))
+    def regra020(self):
+        self.insuranceScore -= 1
+
+    @Rule(Fact(idadeCarro=P(lambda idadeCarro: idadeCarro > 20)))
+    def regra021(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(indice_roubo=P(lambda indice_roubo: indice_roubo > 10)))
+    def regra022(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(cobertura=P(lambda cobertura: cobertura == 2)))
+    def regra023(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(divida=True))
+    def regra024(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(cirurgia=True) & Fact(acompanhamento=True))
+    def regra024(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(cirurgia=True) & Fact(acompanhamento=True))
+    def regra025(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(sangue=P(lambda sangue: sangue == 'O+'))
+          | Fact(sangue=P(lambda sangue: sangue == 'O-')))
+    def regra026(self):
+        self.insuranceScore -= 1
+
+    @Rule(Fact(sangue=P(lambda sangue: sangue == 'AB+'))
+          | Fact(sangue=P(lambda sangue: sangue == 'AB-')))
+    def regra027(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(celiaco_=True) & Fact(acompanhamento=True))
+    def regra028(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(celiaco_=True) & Fact(acompanhamento=True))
+    def regra029(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(hipolactasia_=True) & Fact(acompanhamento=True))
+    def regra030(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(hipolactasia_=True) & Fact(acompanhamento=True))
+    def regra031(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(autoImune=True) & Fact(acompanhamento=True))
+    def regra032(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(autoImune=True) & Fact(acompanhamento=True))
+    def regra033(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(casaPropria=True))
+    def regra034(self):
+        self.declare(Fact(ofereca_seguro_residencial=True))
+
+    @Rule(Fact(casaPropria=True) & Fact(aceitar=True))
+    def regra035(self):
+        self.declare(Fact(ofereca_seguro_carro=True))
+
+    @Rule(Fact(cep=True)
+          | Fact(tombado=True)
+          | Fact(moradiaColetiva=True)
+          | Fact(utilizaMateriaisCombustiveis=True)
+          | Fact(cofre=True)
+          | Fact(servicosEmergenciais=True)
+          | Fact(jaHouveSinistro=True))
+    def regra036(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(areaRiscoNatural=True))
+    def regra037(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(emConstrucao=True)
+          | Fact(sistemaMonitoramento=True))
+    def regra038(self):
+        self.insuranceScore -= 1
+
+    @Rule(Fact(moradia=P(lambda moradia: moradia == 'casa')))
+    def regra039(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(moradia=P(lambda moradia: moradia == 'apartamento')))
+    def regra040(self):
+        self.insuranceScore -= 1
+
+    @Rule(Fact(numeroAparelhosEletronicos_=P(lambda numeroAparelhosEletronicos_: numeroAparelhosEletronicos_ > 10)))
+    def regra041(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(valor_Dos_Bens=P(lambda valor_Dos_Bens: valor_Dos_Bens > 100000)))
+    def regra042(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(transplante=True) & Fact(acompanhamento=True))
+    def regra043(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(transplante=True) & Fact(acompanhamento=False))
+    def regra044(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(LDL=P(lambda LDL: LDL > 200)) & Fact(acompanhamento=True))
+    def regra045(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(LDL=P(lambda LDL: LDL > 200)) & Fact(acompanhamento=False))
+    def regra046(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(cancer_=True) & Fact(acompanhamento=True))
+    def regra047(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(cancer_=True) & Fact(acompanhamento=False))
+    def regra048(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(infarto_=True) & Fact(acompanhamento=True))
+    def regra049(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(infarto_=True) & Fact(acompanhamento=False))
+    def regra050(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(AVC=True) & Fact(acompanhamento=True))
+    def regra051(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(AVC=True) & Fact(acompanhamento=False))
+    def NegarSeguro(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(cardiopatia=True) & Fact(acompanhamento=True))
+    def regra052(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(cardiopatia=True) & Fact(acompanhamento=False))
+    def regra053(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(dst=True) & Fact(acompanhamento=True))
+    def regra054(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(dst=True) & Fact(acompanhamento=False))
+    def regra055(self):
+        self.declare(Fact(seguro_nao_possivel=True))
+
+    @Rule(Fact(sexo_=P(lambda sexo_: sexo_ == 2)) & Fact(gravida_=True))
+    def regra056(self):
+        self.insuranceScore += 1
+
+    @Rule(Fact(planoDeSaude=True))
+    def regra057(self):
+        self.insuranceScore -= 1
